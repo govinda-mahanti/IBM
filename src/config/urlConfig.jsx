91274ee86aca894ce8da0683bb75b2ac2ad24a2c
srutiprusty <sruti.prusty.cse.2022@gmail.com>
2025-08-17 03:19:55 +0530

    url

--- src/config/urlConfig.jsx
+++ src/config/urlConfig.jsx
@@ -1,5 +1,5 @@
 /* export const BASE_URL = "https://ibm-backend-weot.onrender.com";
  */ /* 
 https://ibm-backend-ohmu.onrender.com/ */
-export const BASE_URL = "https://ibm-backend-ohmu.onrender.com/";
+export const BASE_URL = "https://ibm-backend-ohmu.onrender.com";
 /*http://localhost:5000"; */
